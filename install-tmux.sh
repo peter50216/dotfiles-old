443637f55f06978769b1e1e739a5ae7c71f3bd24
Pi-Hsun Shih <peter50216@gmail.com>
2015-11-18 13:10:22 +0800

    some adjust to vim powerline.

--- install-tmux.sh
+++ install-tmux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -v
 # tmux-mem-cpu-load, shik's version.
 
-cd ~
-git clone https://peter50216@bitbucket.org/shik/tmux-mem-cpu-load.git
-cd tmux-mem-cpu-load
-cmake .
-make
-sudo make install
+# cd ~
+# git clone https://peter50216@bitbucket.org/shik/tmux-mem-cpu-load.git
+# cd tmux-mem-cpu-load
+# cmake .
+# make
+# sudo make install
